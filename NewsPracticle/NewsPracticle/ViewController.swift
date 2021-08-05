62f6fb804b67a29eac6b8b89449dbb79d9d6fc22
Sajan Gandhi <sajan.gandhi@ia.ooo>
2021-08-05 10:24:27 +0530

    - initial Project Setup Commit

---
+++ NewsPracticle/NewsPracticle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsPracticle
+//
+//  Created by Dhaval Trivedi on 05/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
